c4eeaaab5d194911fe759ebc821b50d454689b91
unknown <99493565+Larryworker1004@users.noreply.github.com>
2022-02-25 20:57:44 +0100

    touchable container

--- src/features/account/components/chatbot/choosepicture.js
+++ src/features/account/components/chatbot/choosepicture.js
@@ -1,66 +1,72 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-import { colors } from '../../../../infrastructure/theme/colors';
 import Icon from 'react-native-vector-icons/Ionicons';
+import React from 'react';
+import {colors} from '../../../../infrastructure/theme/colors';
 
-const PickPicture = (props) => {
+const PickPicture = props => {
   return (
     <View style={styles.action}>
-          <View style={ { flexDirection: 'row' } }>
-            <View style={styles.toolContainer}>
-              <TouchableOpacity onPress={props.takePhotoFromCamera}>
-                <Icon name="camera-outline" size={20} color="black" />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={props.takePhotoFromCamera}>
-                <Text style={styles.text}>Take Picture</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.toolContainer}>
-              <TouchableOpacity onPress={props.choosePhotoFromLibrary}>
-                <Icon name="image-outline" size={20} color="black" />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={props.choosePhotoFromLibrary}>
-                <Text style={styles.text}>Gallery</Text>
-              </TouchableOpacity>
-            </View>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          onPress={props.takePhotoFromCamera}
+          style={styles.toolContainer}>
+          <View style={styles.center}>
+          <Icon name="camera-outline" size={20} color="black" />
+          <Text style={styles.text}>Take Picture</Text>
           </View>
-        </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={props.choosePhotoFromLibrary}
+          style={styles.toolContainer}>
+          <View style={styles.center}>
+            <Icon name="image-outline" size={20} color="black" />
+            <Text style={styles.text}>Gallery</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 export default PickPicture;
 
 const styles = StyleSheet.create({
-      toolContainer: {
-        backgroundColor: 'white',
-        marginRight: 15,
-        alignContent: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-        borderRadius: 15,
-        width: '45%',
-      },
-      text: {
-        fontSize: 14,
-        fontWeight: '400',
-        color: 'black',
-      },
-      textBold: {
-        fontSize: 16,
-        fontWeight: '900',
-        marginBottom: 20,
-        marginTop: 10,
-        color: 'black',
-      },
-      action: {
-        backgroundColor: colors.bg.quaternary,
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        // height: 100,
-      },
+  toolContainer: {
+    backgroundColor: 'white',
+    marginRight: 15,
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    borderRadius: 15,
+    width: '45%',
+  },
+  center: {
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: 'black',
+  },
+  textBold: {
+    fontSize: 16,
+    fontWeight: '900',
+    marginBottom: 20,
+    marginTop: 10,
+    color: 'black',
+  },
+  action: {
+    backgroundColor: colors.bg.quaternary,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // height: 100,
+  },
 });
--- src/features/account/components/chatbot/select.button.js
+++ src/features/account/components/chatbot/select.button.js
@@ -1,14 +1,14 @@
 /* eslint-disable prettier/prettier */
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
 import React from 'react';
 
 const SelectButton = props => {
   return (
-    <View style={styles.btnContainer}>
-      <TouchableOpacity onPress={props.onPress}>
+      <TouchableOpacity onPress={props.onPress} style={styles.btnContainer}>
         <Text style={styles.text}>{props.text}</Text>
       </TouchableOpacity>
-    </View>
   );
 };
 
